d00b89b44559c387c610ae43744606f4bd713522
lgaliana <lino.galiana@insee.fr>
2024-11-19 15:50:10 +0000

    Appli3 agani

--- utils/getdata.sh
+++ utils/getdata.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/InseeFrLab/formation-bonnes-pratiques-exo-correction.git
 rm -rf ./formation-bonnes-pratiques-exo-correction/.git
-mc cp s3/projet-formation/bonnes-pratiques/data/RPindividus.parquet ./formation-bonnes-pratiques-exo-correction/RPindividus.parquet
+mc cp s3/projet-formation/bonnes-pratiques/data/RPindividus_24.csv RPindividus_24.csv
