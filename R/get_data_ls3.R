2b8bcdd01f62ecff8e092fcc95cbf42690693871
jpramil <julien.pramil@insee.fr>
2024-11-26 09:17:12 +0000

    update get_data_ls3.R

--- R/get_data_ls3.R
+++ R/get_data_ls3.R
@@ -1,9 +1,1 @@
-df <- 
-  aws.s3::s3read_using(
-    FUN = readr::read_csv2,
-    object = "ssplab-formation/rp_2016_individu_sample.csv",
-    bucket = "public/",
-    opts = list("region" = "")
-  )
-
-readr::write_csv2(df, "individu_reg.csv")
+system("mc cp s3/public/ssplab-formation/RPindividus_24.csv RPindividus_24.csv")
